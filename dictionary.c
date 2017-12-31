0b3a0d47610b70cdb267a3fa7625402b4d66f87c
teegreen18 <34717045+teegreen18@users.noreply.github.com>
2017-12-31 17:03:21 -0500

    Add files via upload

--- dictionary.c
+++ dictionary.c
@@ -5,17 +5,34 @@ int main(void) {
   char *buf, *p;
   char arg1[MAXLINE], content[MAXLINE];
   int clientfd;
+  char str[MAXLINE];
+  char word[MAXLINE];
+  char string[MAXLINE];
+  char *rio;
 
   /*get input from user*/
   if ((buf = getenv("QUERY_STRING")) != NULL) {
-    strcpy(arg1, buf);
-    printf(content, "got here");
   }
 
   /*open a connection with the dictionary api in order to get the definition*/
 
-  clientfd = (Open_clientfd(ada.stern.net, 1776));
+  clientfd = (Open_clientfd("services.aonaware.com", "80"));
   Rio_readiinitb(&rio, clientfd);
 
+  strcpy(str, "/DictService/DictService.asmx/Define?");
+  strcat(str, buf);
 
+      /* Make the response body */
+  sprintf(content, buf);
+  sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
+  sprintf(content, "%sThanks for visiting!\r\n", content);
+  
+    /* Generate the HTTP response */
+  printf("Connection: close\r\n");
+  printf("Content-length: %d\r\n", (int)strlen(content));
+  printf("Content-type: text/html\r\n\r\n");
+  printf("%s", content);
+  fflush(stdout);
+
+  exit(0);
 }
